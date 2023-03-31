115607752a88596baa597714a90eb697e375babf
sbj97 <thdqhdwo2@gmail.com>
2023-03-31 09:24:24 +0900

    이미지 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import {Button, Navbar, Container, Nav} from 'react-bootstrap'
 
@@ -7,14 +6,28 @@ function App(){
     <div>
       <Navbar bg="dark" variant="dark">
         <Container>
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Navbar.Brand href="#home">songShop</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#features">shop</Nav.Link>
           <Nav.Link href="#pricing">Pricing</Nav.Link>
         </Nav>
         </Container>
       </Navbar>
+
+      <div className="main-bg"></div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">안녕</div>
+          <img src=""/>
+          <h4>상품명</h4>
+          <p>상품설명</p>
+          <div className="col-md-4">안녕</div>
+          <div className="col-md-4">안녕</div>
+        </div>
+      </div> 
+
     </div>
   )
 }
